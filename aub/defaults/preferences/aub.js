264e9ba8d47c22179cf2f0b1baee97c5a6ef3fd4
Havoc <Havoc@be3fd238-c909-4725-b312-79b88f520c4f>
2005-10-24 22:40:25 +0000

    This commit was generated by cvs2svn to compensate for changes in r2, which included commits to RCS files with non-trunk default branches.
    git-svn-id: http://192.168.1.66/svn/FFext/trunk/ffext1@7 be3fd238-c909-4725-b312-79b88f520c4f

---
+++ aub/defaults/preferences/aub.js
@@ -0,0 +1,6 @@
+pref("@EXTENSION@.name1", "Google Maps");
+pref("@EXTENSION@.name2", "Multimap");
+pref("@EXTENSION@.name3", "Streetmap");
+pref("@EXTENSION@.url1", "http://maps.google.co.uk?q=");
+pref("@EXTENSION@.url2", "http://multimap.com/map/places.cgi?client=public&lang=&advanced=&quicksearch=");
+pref("@EXTENSION@.url3", "http://streetmap.co.uk/newsearch.srf?name=");
